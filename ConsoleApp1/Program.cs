b05e2d45bd84da66b3d9c8782138228b21971037
Ahmedkhaled-ui <ahmad30121998@gmail.com>
2025-06-30 22:18:31 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,135 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+
+Console.WriteLine("Hello, World!");
+// q1
+
+//Console.WriteLine("please inter number");
+//int? num = Convert.ToInt32(Console.ReadLine());
+//Console.Clear();
+//Console.WriteLine( "your num is"+ num);
+
+//q2
+//int num1 = Convert.ToInt16("ahmed");
+//make exapsion
+
+//q3
+// IFloatingPoint<T> num1 = 10,520;
+//const float num2 = 20.2f;
+
+//Console.WriteLine(num1 + num2);
+
+//Q4
+
+//const string name = ("ahmed khaled");
+
+// string cutname = name.Substring(0, 5);
+//Console.WriteLine(name);
+
+//Console.WriteLine(cutname);
+
+
+//Q5
+
+//int a = 15;
+//int b = a;
+
+//b = 4;
+
+//Console.WriteLine(  " a is " + a );
+//Console.WriteLine("b is " + b );
+
+// override
+
+//Q6
+
+//public string person; 
+//string person1 = new person();
+
+//Q7
+
+//Console.WriteLine("Please Enter First Name");
+
+// string Fname = Console.ReadLine();
+
+//Console.WriteLine("Please Enter Second Name");
+//string Sename = Console.ReadLine();
+
+//Console.Clear();
+//Console.WriteLine( Fname + Sename );
+
+
+
+//Q8
+
+//double principal ;
+//double rate;
+//double time;
+
+//Console.WriteLine("Enter Principal");
+
+//principal = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("Enter rate interest");
+
+
+//rate = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("Enter time");
+
+
+//time = Convert.ToDouble(Console.ReadLine());
+
+
+//Console.Clear();
+
+
+//double interest = (principal * rate * time) / 100;
+
+//Console.WriteLine(interest);
+
+
+//Q9
+
+//Console.WriteLine("Please Enter Your Weight");
+//double weight = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("Please Enter Your Height");
+
+//double Height = Convert.ToDouble(Console.ReadLine());
+//Console.Clear();
+//double BMI = (weight)/(weight*Height);
+//Console.WriteLine("Body Mass Index (BMI) = " + BMI);
+
+//Q10
+
+//  int degree = 20  ;
+//string result = degree < 10 ? "Just Cold" : degree > 30 ? "Just Hot" : "Just Good";
+//Console.WriteLine(result);
+
+//Q11
+
+//Console.Write("Enter day: ");
+//int day = Convert.ToInt32(Console.ReadLine());
+
+//Console.Write("Enter month: ");
+//int month = Convert.ToInt32(Console.ReadLine());
+
+//Console.Write("Enter year: ");
+//int year = Convert.ToInt32(Console.ReadLine());
+//Console.Clear();
+//Console.WriteLine($"Today’s date : {day} , {month} , {year}");
+//Console.WriteLine($"Today's date : {day} / {month} / {year}");
+//Console.WriteLine($"Today's date : {day} – {month} – {year}");
+
+//Q12
+
+//c)	 The event is on 06/14/2024
+
+//Q13
+//f)	A value 1 will be assigned to d.
+
+//Q14
+//a)	6.5 1
+//Q15
+// 7 7
